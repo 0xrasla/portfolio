01ee95deb0d5ddbb12685115c9de22ef1b098cfb
rasla <programmer.rasla@gmail.com>
2026-05-07 07:57:39 +0530

    feat: update page content with personalized greeting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<h1>I love you sorna</h1>
+<p>Hello i am rasla</p>
